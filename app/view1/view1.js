74ed66d78ec2256ea8080723b71d7853b72a2ce3
Amit <amit.harlev@gmail.com>
2016-05-16 13:09:07 -0700

    Google Image Search stealing works

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,31 +1,50 @@
 'use strict';
 
 var keywords=[];
-var paramsArray = {};
-        paramsArray.q = ""; // search text
-        paramsArray.num = 1; // integer value range between 1 to 10 including
-        paramsArray.start = 2; // integer value range between 1 to 101, it is like the offset
-        paramsArray.imgSize = "medium"; // for image size
-        paramsArray.imgType = "photo";
-        paramsArray.searchType = "image"; // compulsory 
-        //params.fileType = "jpg"; // you can leave these if extension does not matters you
-        paramsArray.key = "AIzaSyC4TaZPVwxA39f1Cg778szPUjyYa0-X7o8"; // API_KEY got from https://console.developers.google.com/
-        paramsArray.cx = "013248736876494194515:atn_t6mqxyo"; // cx value is the custom search engine value got from https://cse.google.com/cse(if not created then create it)
 
+angular.module('myApp.view1', ['ngRoute'])
 
-        angular.module('myApp.view1', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/view1', {
+		templateUrl: 'view1/view1.html',
+		controller: 'View1Ctrl'
+	});
+}])
 
-        .config(['$routeProvider', function($routeProvider) {
-        	$routeProvider.when('/view1', {
-        		templateUrl: 'view1/view1.html',
-        		controller: 'View1Ctrl'
-        	});
-        }])
+.controller('View1Ctrl', ['$scope','$http',function($scope,$http) {
+	$scope.imgSrc = [];
+	$scope.CreateMural = function(){
 
-        .controller('View1Ctrl', ['$scope','$http',function($scope,$http) {
-        	$scope.imgSrc = [];
-        	$scope.CreateMural = function(){
-        		if($scope.Keyword1!=null)
+		if($scope.Keyword1!=null)
+			keywords.push($scope.Keyword1);
+		if($scope.Keyword2!=null)
+			keywords.push($scope.Keyword2);
+		if($scope.Keyword3!=null)
+			keywords.push($scope.Keyword3);
+		if($scope.Keyword4!=null)
+			keywords.push($scope.Keyword4);
+		if($scope.Keyword5!=null)
+			keywords.push($scope.Keyword5);
+		if($scope.Keyword6!=null)
+			keywords.push($scope.Keyword6);
+
+		if(keywords!=null){
+			for(var i=0;i<keywords.length;i++){
+				$http({
+					method: 'GET',
+					url: 'https://www.google.com/search?q=' + keywords[i] + '&tbm=isch'
+				}).then(function successCallback(response) {
+					var splittext = response.data;
+					var newsplittext = splittext.split('"ou":"');
+					var tempString = newsplittext[1];
+					var linkF = tempString.split('"');
+					$scope.imgSrc.push(linkF[0]);
+				}, function errorCallback(response) {
+					alert(response.data);
+				});
+			}
+		}
+        		/*if($scope.Keyword1!=null)
         			keywords.push($scope.Keyword1);
         		if($scope.Keyword2!=null)
         			keywords.push($scope.Keyword2);
@@ -51,6 +70,6 @@ var paramsArray = {};
         					alert(response.data);
         				});
         			}
-        		}
+        		}*/
         	}
         }]);
