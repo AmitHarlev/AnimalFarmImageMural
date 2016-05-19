1763aee2b20f98430348e0c648e50ef10a7ecd1c
Amit <amit.harlev@gmail.com>
2016-05-19 12:08:11 -0700

    Character buttons set not working yet

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,6 +12,7 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', ['$scope','$http',function($scope,$http) {
 	$scope.CreateMural = function(){
 		$scope.CollageTitle;
+		$scope.HeaderVisible=true;
 		var keywords=[];
 		$scope.imgSrc = [];
 
@@ -45,5 +46,77 @@ angular.module('myApp.view1', ['ngRoute'])
 			}
 		}
 		
+	}
+	$scope.createNapoleon = function(){
+		$scope.CollageTitle = "Napoleon";
+		$scope.Keyword1 = "Pig";
+		$scope.Keyword2 = "Communist";
+		$scope.Keyword3 = "Violent";
+		$scope.Keyword4 = "Dictator";
+		$scope.Keyword5 = "Treacherous";
+		$scope.Keyword6 = "Cruel";
+	}
+	$scope.createSnowball = function(){
+		$scope.CollageTitle = "Snowball";
+		$scope.Keyword1 = "Pig";
+		$scope.Keyword2 = "Passionate";
+		$scope.Keyword3 = "Intelligent";
+		$scope.Keyword4 = "Eloquent";
+		$scope.Keyword5 = "Treacherous";
+		$scope.Keyword6 = "Cruel";
+	}
+	$scope.createMajor = function(){
+		$scope.CollageTitle = "Major";
+		$scope.Keyword1 = "Donkey";
+		$scope.Keyword2 = "Communist";
+		$scope.Keyword3 = "Violent";
+		$scope.Keyword4 = "Dictator";
+		$scope.Keyword5 = "Treacherous";
+		$scope.Keyword6 = "Cruel";
+	}
+	$scope.createBoxer = function(){
+		$scope.CollageTitle = "Boxer";
+		$scope.Keyword1 = "Horse";
+		$scope.Keyword2 = "Communist";
+		$scope.Keyword3 = "Violent";
+		$scope.Keyword4 = "Dictator";
+		$scope.Keyword5 = "Treacherous";
+		$scope.Keyword6 = "Cruel";
+	}
+	$scope.createSquealer = function(){
+		$scope.CollageTitle = "Squealer";
+		$scope.Keyword1 = "Pig";
+		$scope.Keyword2 = "Communist";
+		$scope.Keyword3 = "Violent";
+		$scope.Keyword4 = "Dictator";
+		$scope.Keyword5 = "Treacherous";
+		$scope.Keyword6 = "Cruel";
+	}
+	$scope.createJones = function(){
+		$scope.CollageTitle = "Mr. Jones";
+		$scope.Keyword1 = "Human";
+		$scope.Keyword2 = "Communist";
+		$scope.Keyword3 = "Violent";
+		$scope.Keyword4 = "Dictator";
+		$scope.Keyword5 = "Treacherous";
+		$scope.Keyword6 = "Cruel";
+	}
+	$scope.createClover = function(){
+		$scope.CollageTitle = "Clover";
+		$scope.Keyword1 = "Horse";
+		$scope.Keyword2 = "Communist";
+		$scope.Keyword3 = "Violent";
+		$scope.Keyword4 = "Dictator";
+		$scope.Keyword5 = "Treacherous";
+		$scope.Keyword6 = "Cruel";
+	}
+	$scope.createMoses = function(){
+		$scope.CollageTitle = "Moses";
+		$scope.Keyword1 = "Raven";
+		$scope.Keyword2 = "Communist";
+		$scope.Keyword3 = "Violent";
+		$scope.Keyword4 = "Dictator";
+		$scope.Keyword5 = "Treacherous";
+		$scope.Keyword6 = "Cruel";
 	}
 }]);
